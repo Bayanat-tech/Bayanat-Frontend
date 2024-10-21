ccac25243b823dec86eb8c617dec85423d7c145e
pankajswamivaishnav-rampwin <pankaj.vaishnav@rampwin.com>
2024-10-21 14:42:24 +0530

    make activity table and frontend

--- src/pages/WMS/ActivityBillingPage.tsx
+++ src/pages/WMS/ActivityBillingPage.tsx
@@ -1,9 +1,20 @@
-import { useState } from 'react';
+import {useMemo, useState, useEffect } from 'react';
 import { TUniversalDialogProps } from 'types/types.UniversalDialog';
 import UniversalDialog from 'components/popup/UniversalDialog';
-import { Button } from '@mui/material';
+import { Button, Checkbox } from '@mui/material';
 import Autocomplete from '@mui/material/Autocomplete';
 import TextField from '@mui/material/TextField';
+import { ColumnDef, RowSelectionState } from '@tanstack/react-table';
+import CustomDataTable, {rowsPerPageOptions} from 'components/tables/CustomDataTables';
+import { TBillingActivity } from './types/billingActivity-wms.types';
+import { TAvailableActionButtons } from 'types/types.actionButtonsGroups';
+import ActionButtonsGroup from 'components/buttons/ActionButtonsGroup';
+import { useQuery } from '@tanstack/react-query';
+import { ISearch } from 'components/filters/SearchFilter';
+import WmsSerivceInstance from 'service/service.wms';
+import { useSelector } from 'store';
+import { getPathNameList } from 'utils/functions';
+import useAuth from 'hooks/useAuth';
 
 const ActivityBillingPage = () => {
   const [addActivityFormPopup, setActivityFormPopup] = useState<TUniversalDialogProps>({
@@ -15,7 +26,10 @@ const ActivityBillingPage = () => {
     title: 'Add ActivityBilling',
     data: { existingData: {}, isEditMode: false }
   });
-  const toggleCountryPopup = () => {
+  const toggleActivityPopup = (refetchData?: boolean) => {
+    if (addActivityFormPopup.action.open === true && refetchData) {
+      refetchActivityBillingData();
+    }
     setActivityFormPopup((prev) => {
       return { ...prev, data: { isEditMode: false, existingData: {} }, action: { ...prev.action, open: !prev.action.open } };
     });
@@ -25,12 +39,100 @@ const ActivityBillingPage = () => {
       return { ...prev, data: { isEditMode: false, existingData: {} }, action: { ...prev.action, open: !prev.action.open } };
     });
   };
+  
+  
+  // For Activity Billing Table
+  const { permissions, user_permission } = useAuth();
+  const [paginationData, setPaginationData] = useState({ page: 0, rowsPerPage: rowsPerPageOptions[0] });
+  const { app } = useSelector((state: any) => state.menuSelectionSlice);
+  const [searchData, setSearchData] = useState<ISearch>();
+  const pathNameList = getPathNameList(location.pathname);
+  const [toggleFilter, setToggleFilter] = useState<boolean | null>(null);
+  const [rowSelection, setRowSelection] = useState<RowSelectionState>({});
+  const handleActions = (actionType: string, rowOriginal: TBillingActivity) => {
+    actionType === 'edit' && handleEditActivityBilling(rowOriginal);
+  };
+  const handleEditActivityBilling = (existingData: TBillingActivity) => {
+    setActivityFormPopup((prev) => {
+      return {
+        action: { ...prev.action, open: !prev.action.open },
+        title: 'Edit Activity',
+        data: { existingData, isEditMode: true }
+      };
+    });
+  };
+  const handleChangePagination = (page: number, rowsPerPage: number) => {
+    setPaginationData({ page, rowsPerPage });
+  };
+  
+  const columns = useMemo<ColumnDef<TBillingActivity>[]>(
+    () => [
+      {
+        id: 'select-col',
+        header: ({ table }) => (
+          <Checkbox
+            checked={table.getIsAllRowsSelected()}
+            indeterminate={table.getIsSomeRowsSelected()}
+            onChange={table.getToggleAllRowsSelectedHandler()}
+          />
+        ),
+        cell: ({ row }) => (
+          <Checkbox checked={row.getIsSelected()} disabled={!row.getCanSelect()} onChange={row.getToggleSelectedHandler()} />
+        )
+      },
+      {
+        accessorFn: (row) => row.principal_name,
+        id: 'prin_name',
+        header: () => <span>Country Code</span>
+      },
+      {
+        accessorFn: (row) => row.activity_code,
+        id: 'act_code',
+        header: () => <span>Country Name</span>
+      },
+      {
+        accessorFn: (row) => row.activity_name,
+        id: 'activity',
+        header: () => <span>Country GCC</span>
+      },
+      {
+        accessorFn: (row) => row.job_type,
+        id: 'job_type',
+        header: () => <span>Company Code</span>
+      },
+      {
+        id: 'actions',
+        header: () => <span>Actions</span>,
+        cell: ({ row }) => {
+          const actionButtons: TAvailableActionButtons[] = ['edit'];
 
- 
-  
-  
+          return <ActionButtonsGroup handleActions={(action) => handleActions(action, row.original)} buttons={actionButtons} />;
+        }
+      }
+    ],
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    []
+  );
+
+  //------------------useQuery----------------
+    const {
+      data: activityBillingData,
+      isFetching: isActivityFetchLoading, 
+      refetch: refetchActivityBillingData
+    } = useQuery ({
+      queryKey: ['activity_billing_data', searchData, paginationData],
+      queryFn:()=> WmsSerivceInstance.getMasters(app, pathNameList[pathNameList.length - 1], paginationData, searchData),
+      enabled: user_permission?.includes(permissions?.[app.toUpperCase()]?.children[pathNameList[3]?.toUpperCase()]?.serial_number)
+    })
+   
+  //------------------useEffect----------------
+  useEffect(() => {
+    setSearchData(null as any);
+    setToggleFilter(null as any);
+  }, []);
   return (
     <div className="w-full">
+      {/*  */}
       <div className="w-full flex justify-between">
         <div className="ba_principal_search_div">
           <Autocomplete
@@ -55,13 +157,24 @@ const ActivityBillingPage = () => {
         </div>
       </div>
       <div className="w-full">
-        dummy table
+       <CustomDataTable
+         rowSelection={rowSelection}
+         setRowSelection={setRowSelection}
+         row_id="act_code"
+         columns={columns}
+         data={activityBillingData?.tableData || []}
+        count={activityBillingData?.count}
+        onPaginationChange={handleChangePagination}
+        isDataLoading={isActivityFetchLoading}
+        toggleFilter={toggleFilter}
+        hasPagination={true}
+       />
       </div>
       {/* Add Activity Dialogue Box */}
       {!!addActivityFormPopup && addActivityFormPopup.action.open && (
         <UniversalDialog
           action={{ ...addActivityFormPopup.action }}
-          onClose={toggleCountryPopup}
+          onClose={toggleActivityPopup}
           title="Billing Activity Form"
           hasPrimaryButton={false}
         >
